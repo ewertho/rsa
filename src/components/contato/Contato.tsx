d1eb35ee10bba97b65701c437fe9a1ed3be107ae
Ewerton <ewerton.lfb@gmail.com>
2025-06-05 16:42:41 -0300

    retirada de numero de contato 21977030205

--- src/components/contato/Contato.tsx
+++ src/components/contato/Contato.tsx
@@ -141,27 +141,6 @@ const Contato = () => {
                     21 96401-5417
                   </div>
                 </div>
-                <div className="flex items-center mt-2 text-gray-600 dark:text-gray-400">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    viewBox="0 0 24 24"
-                    className="w-8 h-8 text-gray-500"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="1.5"
-                      d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                    />
-                  </svg>
-                  <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                    21 97703-0205
-                  </div>
-                </div>
               </div>
 
               <form
